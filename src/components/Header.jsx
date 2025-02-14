1f32b4cc3e9552c9445e9e89623955a9b18850da
omar mahmoud <omarmahmoudcs@gmail.com>
2025-02-14 15:06:28 +0200

    finish landing component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,28 +1,31 @@
 import React, { useEffect, useRef, useState } from "react";
 import { throttle } from "lodash";
-import './Header.css'
+import "./Header.css";
 const Header = () => {
-  const headerRef = useRef(null)
+  const headerRef = useRef(null);
   const [links, setLinks] = useState(["Features", "Team", "Sign In"]);
 
-  useEffect(()=>{
-    const handleScroll = throttle(()=> {
+  useEffect(() => {
+    const handleScroll = throttle(() => {
       if (window.scrollY > 100) {
         headerRef.current.classList.add("scrolled");
       } else {
         headerRef.current.classList.remove("scrolled");
       }
-    }, 100)
+    }, 100);
 
-    window.addEventListener('scroll', handleScroll)
+    window.addEventListener("scroll", handleScroll);
 
     return () => {
-      window.removeEventListener('scroll', handleScroll)
-    }
+      window.removeEventListener("scroll", handleScroll);
+    };
   }, []);
   return (
-    <header ref={headerRef} className="header-initial fixed top-0 left-0 w-full z-50 transition-all duration-200">
-      <div className="container mx-auto flex justify-between items-center gap-[30px] sm:gap-0 flex-col sm:flex-row">
+    <header
+      ref={headerRef}
+      className="header-initial fixed top-0 left-0 w-full z-50 transition-all duration-200"
+    >
+      <div className="container flex justify-between items-center gap-[30px] sm:gap-0 flex-col sm:flex-row">
         <a href="/">
           <img src="/src/assets/images/logo.svg" alt="logo-img" />
         </a>
---
+++ src/components/Landing.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+
+const Landing = () => {
+  return (
+    <section className="bg-[#1C2230]">
+      <div className="container">
+        <div className="element-center flex-col pt-[200px]">
+          <div className="w-[750px] max-w-full">
+            <img
+              src="/src/assets/images/illustration-intro.png"
+              alt="landing-img"
+              className="w-full h-fit"
+            />
+          </div>
+          <div className="text-white text-center">
+            <h1 className="text-[30px] md:text-[40px] font-semibold mb-[15px]">
+              All your files in one secure location,
+              <br />
+              accessible anywhere
+            </h1>
+            <p className="font-normal text-lg px-[15px] md:w-[600px] max-w-full mb-8 md:mx-auto">
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+              Reprehenderit, eos eveniet voluptatibus laudantium fuga quasi
+              consectetur magni a nam iure ratione tempore beatae eaque et
+              labore magnam aperiam sequi eligendi.
+            </p>
+          </div>
+          <a href="/" className="btn w-[280px] h-[60px] rounded-[30px] element-center text-white font-medium">Get Started</a>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Landing;
