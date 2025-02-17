8f3cfb47a5c2e4963b14b998c2d4ac3867eabcac
omar mahmoud <omarmahmoudcs@gmail.com>
2025-02-17 18:48:45 +0200

    finish GetStarted Component

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,6 +3,7 @@ import Landing from '../components/Landing'
 import Features from '../components/Features'
 import StayProductive from '../components/StayProductive'
 import Testimonials from '../components/Testimonials'
+import GetStarted from '../components/GetStarted'
 
 const Home = () => {
   return (
@@ -11,6 +12,7 @@ const Home = () => {
       <Features />
       <StayProductive />
       <Testimonials />
+      <GetStarted />
     </>
   )
 }
